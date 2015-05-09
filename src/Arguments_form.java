da0cc5b8e4ca00c4f39ac4c133c254dc7d5bb641
= <f492015865@gmail.com>
2015-05-09 23:49:14 +1000

    Date picker fixed

--- src/Arguments_form.java
+++ src/Arguments_form.java
@@ -22,7 +22,8 @@ import javax.swing.SpringLayout;
 public class Arguments_form {
 	JPanel p; 
 	JFXPanel fxPanel = new JFXPanel();
-	public DatePicker checkInDatePicker;
+	public DatePicker checkInDatePicker_from;
+	public DatePicker checkInDatePicker_to;
 	//public Stage stage;
 	private final String datePattern = "dd-MMM-yyyy";
 	private int window = 3;
@@ -30,7 +31,7 @@ public class Arguments_form {
 	private JTextField window_field = new JTextField(10);
 	private JTextField threshold_field = new JTextField(10);
 	
-	public Arguments_form() {
+	public Arguments_form(JPanel card) {
 	    String[] labels = {"Window: ", "Threshold: "};
 	    int numPairs = labels.length;
 	    
@@ -40,11 +41,11 @@ public class Arguments_form {
 	    
 	    VBox vbox = new VBox(20);
         vbox.setStyle("-fx-padding: 10;");
-        Scene scene = new Scene(vbox, 150, 40);
+        Scene scene = new Scene(vbox, 250, 100);
         //stage.setScene(scene);
-        
-        checkInDatePicker = new DatePicker();
-        StringConverter converter = new StringConverter<LocalDate>() {
+        //Date from picker*************************************************************
+        checkInDatePicker_from = new DatePicker();
+        StringConverter converter_from = new StringConverter<LocalDate>() {
             DateTimeFormatter dateFormatter = 
                 DateTimeFormatter.ofPattern(datePattern);
             @Override
@@ -64,19 +65,53 @@ public class Arguments_form {
                 }
             }
         };             
-        checkInDatePicker.setConverter(converter);
-        //checkInDatePicker.setPromptText(datePattern.toLowerCase());
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+        checkInDatePicker_from.setConverter(converter_from);
+        //checkInDatePicker_from.setPromptText(datePattern.toLowerCase());
+        GridPane gridPane_from = new GridPane();
+        gridPane_from.setHgap(10);
+        gridPane_from.setVgap(10);
 
-        Label checkInlabel = new Label("Start Date:");
-        gridPane.add(checkInlabel, 0, 0);
+        Label checkInlabel_from = new Label("Start Date:");
+        gridPane_from.add(checkInlabel_from, 0, 0);
 
-        GridPane.setHalignment(checkInlabel, HPos.LEFT);
-        gridPane.add(checkInDatePicker, 0, 1);
+        GridPane.setHalignment(checkInlabel_from, HPos.LEFT);
+        gridPane_from.add(checkInDatePicker_from, 0, 1);
         
-        vbox.getChildren().add(checkInDatePicker);
+        //Date to picker**********************************************************
+        checkInDatePicker_to = new DatePicker();
+        StringConverter converter_to = new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = 
+                DateTimeFormatter.ofPattern(datePattern);
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        };             
+        checkInDatePicker_to.setConverter(converter_to);
+        //checkInDatePicker_from.setPromptText(datePattern.toLowerCase());
+        GridPane gridPane_to = new GridPane();
+        gridPane_to.setHgap(10);
+        gridPane_to.setVgap(10);
+
+        Label checkInlabel_to = new Label("End Date:");
+        gridPane_to.add(checkInlabel_to, 1, 0);
+
+        GridPane.setHalignment(checkInlabel_to, HPos.LEFT);
+        gridPane_to.add(checkInDatePicker_to, 1, 1);
+        //**********************************************
+        vbox.getChildren().addAll(new Label("Start"),checkInDatePicker_from, checkInDatePicker_to);
 	    
         fxPanel.setScene(scene);
         //stage.show();
@@ -84,7 +119,6 @@ public class Arguments_form {
 	    
 	    //Create and populate the panel.
 	    p = new JPanel(new SpringLayout());
-	    p.add(fxPanel);
 	    JLabel window_label = new JLabel("Window: ", JLabel.TRAILING);
 	    p.add(window_label);
 	    window_label.setLabelFor(window_field);
@@ -94,7 +128,8 @@ public class Arguments_form {
 	    p.add(threshold_label);
 	    threshold_label.setLabelFor(threshold_field);
 	    p.add(threshold_field);
-	   
+
+	    card.add(fxPanel);
 	    //Lay out the panel.
 	    SpringUtilities.makeCompactGrid(p,
 	                                    numPairs, 2, //rows, cols
